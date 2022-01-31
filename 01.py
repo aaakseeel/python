d5a8810dcc5d14ff96c2d2eb118c4c1adfc6909a
akselpoiss <98739357+akselpoiss@users.noreply.github.com>
2022-01-31 11:37:43 +0200

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,46 @@
+# harjutus 01
+# aksel ratt
+# 31.01.22
+
+print("\"Cabarnet Sauvignon\" või 'Cabarnet Sauvignon'?")
+
+print("  @..@")
+print(" (----)")
+print("( \__/ )")
+print('^^ "" ^^')
+
+a=int(input('Sisesta ruudu külje pikkus: '))
+s=a*a
+print('vastus: ruudu pindala on',s,'cm^2')
+
+
+'''
+nimi=input('lisa oma nimi: ')
+print('tere', nimi+,'!') # pluss ei lisa tühikut
+vanus=int(input('lisa oma vanus: ')
+'''
+          
+vanuste_vahe=17-vanus
+print('meie vanuste vahe on:',vansute_vahe,'aastat')
+
+'''
+a = 12
+b = 16
+s=a*b
+p = 2*(a+b)
+print('pindala',s,'ruutmeetrit')
+print(ümbermõõt',p,'meetrit')
+print(p)
+#S=a*b
+print(2*3)
+'''
+
+'''
+print("tere maailm") #sõne
+print('tere sa ka') #string
+print(2000) #int / täisarv
+print(2.5) #float / komaarv
+print('ütle 'tere maailm"')
+print("ütle \"tere maailm\"") #escape sting \n
+print(type(2000)) # näitab andmetüüpi
+'''
