7e5d979452f96281b5281131e878babfa1fccbcb
aksel ratt <98739357+akselpoiss@users.noreply.github.com>
2022-02-03 09:59:45 +0200

    Add files via upload

---
+++ 04.py
@@ -0,0 +1,31 @@
+#h04
+#a. "hitler" ratt
+#03.02.1945
+
+'''
+#matemaatika
+arv1=('')
+arv2=('')
+tehe=input('vali tehe:\n1) liitmine\n2) lahutamine jne')
+
+if tehe==1:
+    teen seda
+elif tehe==2:
+    teen seda
+elif= tehe==3:
+    teen seda
+else:
+    teen
+
+#ruudu kontroll
+a=int(input('sisesta ruudu üks külg:'))
+b=int(input('sisesta ruudu teine külg:'))
+if a==b:
+    print('tegemist on ruuduga')
+else:
+    print('risttahukas')
+'''
+
+#juubel
+sunnipaev=input('kirjuta sünnipäev:')
+juubel=
