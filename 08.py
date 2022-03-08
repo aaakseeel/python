d85162a09661dafcb85be5df91533cae471cf812
aksel ratt <98739357+akselpoiss@users.noreply.github.com>
2022-03-08 10:26:14 +0200

    Add files via upload

--- 08.py
+++ 08.py
@@ -1,11 +1,28 @@
-#h8
+#ül08
 #aksel ratt
-#02.03.22
+#08.03.22
 
-#auto klass
 class auto:
+    #atribuudid
     mark='teadmata'
-    aasta='teadmata'
+    aasta=0
     hind='teadmata'
-    varv='teadmata'
-    kiirus='teadmata'
+    
+    #meetodid
+    def lisamark(self,a):
+        self.mark=a
+        
+    def lisaaasta(self,b):
+        self.aasta=b
+        
+    def lisahind(self,c):
+        self.hind=c
+        
+    def kuva(self):
+        print('mark: {} \naasta: {} \nhind:{}'.format(self.mark, self.aasta,self.hind))
+        
+auto()
+auto.lisamark('mersu')
+auto.lisaasta('2000')
+auto.lisahind('3000')
+auto.kuva()
